050d39707a206c77d418ba81f07e75e35c0951c9
Moises Meirelles <mshmeirelles@gmail.com>
2016-10-20 13:10:18 -0200

    url blog

--- blog/pelicanconf.py
+++ blog/pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Moisés Meirelles'
 SITENAME = 'mshmeirelles blog'
-SITEURL = 'mshmeirelles.github.io/blog/output'
+SITEURL = 'moisesmeirelles.com.br/blog/output'
 
 PATH = 'content'
 
